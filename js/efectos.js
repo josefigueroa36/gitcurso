b231373acaf1484a00bf0d9fef9368f538b98f8e
Jose <jbfigueroa2000@gmail.com>
2021-08-31 22:56:18 -0500

    empezando de 0

--- js/efectos.js
+++
@@ -1,7 +0,0 @@
-const i=document.getElementById('i')
-
-document.getElementById('body').addEventListener('click',()=>{
-  i.classList.toggle('trans')
-})
-sessionStorage.setItem('name','miguel')
-console.log(sessionStorage.getItem('name'))
--- js/menu.js
+++
@@ -1,8 +0,0 @@
-const btn=document.getElementById('btnmenu'),
-      menu=document.getElementById('menu')
-      
-      
-      
-      btn.addEventListener('click',()=>{
-        menu.classList.toggle('menuActive')
-      })
